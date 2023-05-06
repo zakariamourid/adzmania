53230d09fd2994ed58675b13d4d85ec61b3ceb3a
ZAKARIAE MOURID <mourid.zakariae@ofppt-edu.ma>
2023-05-06 14:42:49 +0100

    first commit

--- adzmania_back/routes/api.php
+++ adzmania_back/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/signup', [AuthController::class,"signup"]);
-Route::post('/logout', [AuthController::class,"logout"]);
-Route::get('/users', [AuthController::class,"getUsers"]);
